a309568f920c7830c0353291cf3832ec890ef613
JeffryGonzalez <jeff@hypertheory.com>
2021-08-03 11:33:35 -0400

    ready for an API

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { TodosDataService } from 'src/app/services/todos.data-service';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  myPay = 150_000;
+
+  numberOfTodos$!: Observable<number>;
+  constructor(private service: TodosDataService) { }
 
   ngOnInit(): void {
+    this.numberOfTodos$ = this.service.getTodoItems()
+      .pipe(
+        map(todos => todos.length)
+      );
   }
 
 }
